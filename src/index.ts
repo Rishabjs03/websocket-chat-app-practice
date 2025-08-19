266787020b73399da4aad1a1a9061c2d2ade47bc
Rishabjs03 <rishabagarwaljs@gmail.com>
2025-08-19 14:52:14 +0530

    done with the backend and room logic

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,36 @@ import { WebSocketServer, WebSocket } from "ws"
 
 const wss = new WebSocketServer({ port: 8080 })
 
-let UserCount = 0;
-let allSockets: WebSocket[] = [];
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
+let allSockets: User[] = [];
 wss.on('connection', (socket) => {
-    allSockets.push(socket)
-
-    console.log("user connected");
-    UserCount = UserCount + 1;
-
-
     socket.on('message', (message) => {
-        console.log("message received" + message.toString())
-        for (let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            s?.send(message.toString())
+        // @ts-ignore
+        const ParsedMessage = JSON.parse(message);
+        if (ParsedMessage.type === "join") {
+            allSockets.push({
+                socket,
+                room: ParsedMessage.payload.roomId,
+            })
+
         }
+        if (ParsedMessage.type === "chat") {
+            // 1. Find which room the current user (who sent the message) is in
+            const currentUserRoom = allSockets.find((x) => x.socket === socket)?.room;
+
+            // 2. Send the message only to sockets in the same room
+            for (let i = 0; i < allSockets.length; i++) {
+                const s = allSockets[i];
+                if (s?.room === currentUserRoom) {
+                    s?.socket.send(ParsedMessage.payload.message);
+                }
+            }
+        }
+
     })
+
 })
